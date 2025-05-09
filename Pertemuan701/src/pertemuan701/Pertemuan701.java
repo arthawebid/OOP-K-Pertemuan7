5a1d69cf99e3ce77b32f90a4152179f1a2374c98
OOP-K <arthawebid@gmail.com>
2025-05-09 11:17:28 +0800

    Pertemuan 7

---
+++ Pertemuan701/src/pertemuan701/Pertemuan701.java
@@ -0,0 +1,17 @@
+package pertemuan701;
+
+/**
+ *
+ * @author Made Artha
+ * TGL 09 Mei 2025
+ */
+public class Pertemuan701 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
